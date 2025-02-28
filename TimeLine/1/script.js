065cb4b4a52034384c2913628f7700478ad5ef77
Soham Bhandary <bhandarysoham27@gmail.com>
2025-02-28 21:58:09 +0530

    Initial commit

---
+++ TimeLine/1/script.js
@@ -0,0 +1,29 @@
+var t1=gsap.timeline()
+
+
+t1.to("#box1",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:3,
+    
+
+
+})
+t1.to("#box2",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:3,
+  
+
+
+})
+t1.to("#box3",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:3,
+
+
+})
