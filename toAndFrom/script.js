ff015a563120313f5149f5917b4d62ea5556822a
Soham Bhandary <bhandarysoham27@gmail.com>
2025-02-28 22:00:37 +0530

    Initial commit

---
+++ toAndFrom/script.js
@@ -0,0 +1,18 @@
+// gsap.to("#box",{
+//     x:1000,
+//     rotate:360,
+//     backgroundColor:"red",
+    
+//     duration:2,
+//     delay:1
+// }) //Initail to Final
+gsap.from("#box",{
+    x:1000,
+    rotate:360,
+    backgroundColor:"red",
+    
+    duration:2,
+    delay:1
+}) // Final to Initial
+
+
